c1fc9aa08917437eb8d631ee41cad002e6989344
jakareya76 <jakareya1306@gmail.com>
2024-05-31 12:11:52 +0600

    add optional chaining in useQuery for user email

--- src/pages/Dashboard/PaymentHistory.jsx
+++ src/pages/Dashboard/PaymentHistory.jsx
@@ -7,7 +7,7 @@ const PaymentHistory = () => {
   const axiosSecure = useAxiosSecure();
 
   const { data: payments = [] } = useQuery({
-    queryKey: ["payments", user.email],
+    queryKey: ["payments", user?.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/payments/${user?.email}`);
       return res.data;
